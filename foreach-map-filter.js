4e4f6aab622e4e42522048a8e95217f16bd52887
Adam Marley <141704661+marleyad@users.noreply.github.com>
2024-10-03 20:51:42 -0400

    Update foreach-map-filter.js

--- foreach-map-filter.js
+++ foreach-map-filter.js
@@ -207,7 +207,7 @@ function removeVowels(str) {
 }
 
 /*
-Write a function called doubleOddNumbers which accepts an array and returns a new array with all of the odd numbers doubled (HINT - you can use map and filter to double and then filter the odd numbers).
+Write a function called doubleOddNumbers. It accepts an array and returns a new array with all of the odd numbers doubled (HINT - you can use map and filter to double and then filter the odd numbers).
 
 Examples:
     doubleOddNumbers([1,2,3,4,5]) // [2,6,10]
